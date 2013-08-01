52b0f3fdb4515d89367c9bcf73acbd250e3ae2d6
David Prause <praused@hotmail.com>
2013-08-01 12:59:11 -0400

    downloaded example - no changes

---
+++ C#/ProductStore/Models/ProductInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProductStore.Models
+{
+    public class ProductInitializer : DropCreateDatabaseIfModelChanges<ProductContext>
+    {
+        protected override void Seed(ProductContext context)
+        {
+            context.Products.Add(new Product { Name = "Tomato soup", Category = "Groceries", Price = 1.39M });
+            context.Products.Add(new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M });
+            context.Products.Add(new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M });
+        }
+    }
+}
---
+++ C#/ProductStore/Models/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace ProductStore.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ProductContext db = new ProductContext();
+
+        public ProductRepository()
+        {
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products;
+        }
+
+        public Product Get(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        public Product Add(Product item)
+        {
+            db.Products.Add(item);
+            db.SaveChanges();
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            Product product = db.Products.Find(id); 
+            db.Products.Remove(product); 
+            db.SaveChanges(); 
+        }
+
+        public bool Update(Product item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
